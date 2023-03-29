a03eff43b78e011234836b30b24ec901027fa5f0
ReneOsvaldoCamachoFernandez <124537731+ReneOsvaldoCamachoFernandez@users.noreply.github.com>
2023-03-28 21:11:41 -0400

    desktop and hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 import Image from "next/image";
 import bgMobile from "public/images/bg-mobile.svg";
 import Illustrations from "public/images/illustration-mockups.svg";
 import logo from "public/images/logo.svg";
-
+import { useBreakpoint } from "../useBreakpoint";
+import bgdesktop from "public/images/bg-desktop.svg";
+import React, { useState } from "react";
 export default function Home() {
+  const breakpoint = useBreakpoint();
+  const bgImage = breakpoint.xl === true ? bgdesktop : bgMobile;
+  const [hoverState, setHovered] = useState(false);
+
+  function changeIcon(id: string) {
+    const target = document.getElementById(id);
+    if (target) {
+      target.setAttribute("fill", "#E882E8");
+    }
+  }
+  function setIconNormal(id: string) {
+    const target = document.getElementById(id);
+    if (target) {
+      target.setAttribute("fill", "#FFF");
+    }
+  }
   return (
     <>
       <Head>
@@ -14,34 +31,50 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="images/favicon-32x32.png" />
       </Head>
-      <main className="bg-[#674BAF] relative h-full">
-        <Image src={bgMobile} alt="bg" className="absolute z-0"></Image>
-        <div className="px-[40px] py-[30px] z-10 absolute">
-          <div>
-            <Image src={logo} alt="logo" width={120}></Image>
-            <Image src={Illustrations} alt="illu" className="mt-[60px]"></Image>
+      <main className="bg-[#674BAF] relative h-full ">
+        <Image
+          src={bgImage}
+          alt="bg"
+          className="absolute z-0 xl:h-full"
+        ></Image>
+        <div className="px-[40px] xl:px-[80px] py-[30px] xl:py-[65px] z-10 absolute xl:flex ">
+          <div className="xl:w-[60%]">
+            <Image
+              src={logo}
+              alt="logo"
+              width={breakpoint.xl === true ? 200 : 120}
+            ></Image>
+            <Image
+              src={Illustrations}
+              alt="illu"
+              className="mt-[60px] xl:mt-[80px]"
+            ></Image>
           </div>
-          <div className="flex flex-col items-center text-[#FFF] text-center">
-            <h1 className="font-semibold text-3xl font-Poppins mt-[60px]">
+          <div className="xl:w-[44%] flex flex-col items-center xl:items-start text-[#FFF] text-center xl:text-left">
+            <h1 className="font-semibold text-3xl xl:text-5xl font-Poppins mt-[60px] xl:mt-[180px] ">
               Build The Community Your Fans Will Love
             </h1>
-            <p className="font-normal font-OpenSans mt-[10px] px-[2px]">
+            <p className="font-normal font-OpenSans mt-[10px] px-[2px] xl:mt-[35px] xl:text-xl">
               Huddle re-imagines the way we build communities. You have a voice,
               but so does your audience. Create connections with your users as
               you engage in genuine discussion.
             </p>
-            <button className="bg-[#FFF] text-xs text-[#674BAF] rounded-full px-[75px] py-[12px] mt-[20px]">
+            <button className="bg-[#FFF] hover:bg-[#E882E8] hover:text-[#FFFFFF] text-xs text-[#674BAF] xl:text-xl rounded-full px-[75px] py-[12px] xl:py-[16px] drop-shadow-lg  mt-[20px]">
               Register
             </button>
-            <div className="flex gap-[10px] mt-[60px]">
+            <div className="flex gap-[10px] mt-[60px] xl:absolute xl:right-[80px] xl:bottom-0">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 width={28}
+                className="hover:cursor-pointer"
+                onMouseEnter={() => changeIcon("svg01")}
+                onMouseLeave={() => setIconNormal("svg01")}
               >
                 {/*<!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.*/}
                 <path
-                  fill="#FFF"
+                  id="svg01"
+                  fill={"#FFF"}
                   d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
                 />
               </svg>
@@ -49,10 +82,14 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 width={28}
+                className="hover:cursor-pointer"
+                onMouseEnter={() => changeIcon("svg02")}
+                onMouseLeave={() => setIconNormal("svg02")}
               >
                 {/*<!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->*/}
                 <path
-                  fill="#FFF"
+                  id="svg02"
+                  fill={"#FFF"}
                   d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
                 />
               </svg>
@@ -60,10 +97,14 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 448 512"
                 width={28}
+                className="hover:cursor-pointer"
+                onMouseEnter={() => changeIcon("svg03")}
+                onMouseLeave={() => setIconNormal("svg03")}
               >
                 {/*<!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->*/}
                 <path
-                  fill="#FFF"
+                  fill={"#FFF"}
+                  id="svg03"
                   d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
                 />
               </svg>
